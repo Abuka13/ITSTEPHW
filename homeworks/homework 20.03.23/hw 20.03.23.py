a376ca59193cc2fcbd1e3ef75c1a998a454366fb
Abuka13 <abukaozbekbay@gmail.com>
2023-03-20 00:36:53 +0600

    Update
    Update

--- homeworks/homework 20.03.23/hw 20.03.23.py
+++ homeworks/homework 20.03.23/hw 20.03.23.py
@@ -13,11 +13,18 @@
 #     print("Счастливый")
 # else:
 #     print('Обычный')
-#TODO TASK2
-a = int(input())
-b = int(input())
-c = a
-while c % b != 0:
-    c += a
-print(c)
+# #TODO TASK2
+# a = int(input())
+# b = int(input())
+# c = a
+# while c % b != 0:
+#     c += a
+# print(c)
 #TODO TASK3
+import openpyxl
+from openpyxl import load_workbook
+from openpyxl import Workbook
+wb = load_workbook('data.xlsx')
+sheet = wb.active
+for sheet in wb:
+    print(sheet['A1'].value,sheet['B1'].value,sheet['C1'].value,sep='\n')
