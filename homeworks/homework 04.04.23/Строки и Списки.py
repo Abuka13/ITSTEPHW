d2eaee99a016ac12b9dbc5aed9dbacb1a1951e2f
Abuka13 <abukaozbekbay@gmail.com>
2023-04-14 21:04:20 +0600

    Update
    Update

--- homeworks/homework 04.04.23/Строки и Списки.py
+++ homeworks/homework 04.04.23/Строки и Списки.py
@@ -1,18 +1,23 @@
 #TODO task1
-five = 0
-four = 0
-three = 0
-two = 0
-grades = [5,5,5,5,3,4,5,4,4]
-average = sum(grades) / len(grades)
-for i in range(len(grades)):
-    if grades[i] == 5:
-        five+=1
-    elif grades[i] == 4:
-        four+=1
-    elif grades[i] == 3:
-        three+=1
-    elif grades[i] == 2:
-        two+=1
-print(five,four,three,two)
-print(average)
+# five = 0
+# four = 0
+# three = 0
+# two = 0
+# grades = [5,5,5,5,3,4,5,4,4]
+# average = sum(grades) / len(grades)
+# for i in range(len(grades)):
+#     if grades[i] == 5:
+#         five+=1
+#     elif grades[i] == 4:
+#         four+=1
+#     elif grades[i] == 3:
+#         three+=1
+#     elif grades[i] == 2:
+#         two+=1
+# print(five,four,three,two)
+# print(average)
+#task2
+grades = input()
+grades = grades.replace('2','3')
+
+print(grades)
